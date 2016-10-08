c9a7c6de84bbd84f0aea987507705519494a9409
yaozhao <jimzhaoshine@gmail.com>
2016-10-08 11:29:23 -0400

    add testing scripts

---
+++ tests/testAll.m
@@ -0,0 +1,2 @@
+import YMicroscope.*
+camera = Andor
---
+++ tests/testCameraAndorZyla.m
@@ -0,0 +1,8 @@
+import YMicroscope.*
+%%
+cam = CameraAndorZyla();
+display('camera successfully loaded')
+%%
+cam.setExposure(200);
+display('exposure successfully set')
+%%
